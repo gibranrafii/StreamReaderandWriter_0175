291eea82afdd66826d84e919f68cb7df88f9905f
gibranrafii <gibranrafi21@gmail.com>
2024-06-04 11:06:43 +0700

    Membuat cout dan try pada int main

--- Pertemuan 12.4 (Exeption standard)/Pertemuan 12.4 (Exeption standard).cpp
+++ Pertemuan 12.4 (Exeption standard)/Pertemuan 12.4 (Exeption standard).cpp
@@ -8,5 +8,12 @@ using namespace std;
 
 int main()
 {
-   
+    cout << "Awal Program" << endl; // penanda 1:...
+    try {
+        array<int, 3> data = { 8, 4, 2 };
+        // pesan array integer 3 elemen
+        //cout << data.at(5) << endl;
+        //memanggil array elemen ke 5
+    }
+  
 }
